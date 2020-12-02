90f246081be68228d165f0f65a802e0966acf6c3
SimoneMicheletti <sm.micheletti@gmail.com>
2020-12-02 19:59:12 +0100

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -8,25 +8,28 @@ var app = new Vue ({
 
   methods: {
 
-    searchFilm: function() {
+    searchFilm() {
       this.arrayFilm = [],
-      // API Movie
-      axios.get("https://api.themoviedb.org/3/search/movie?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
-      .then(rispostaMov => this.updateArray(rispostaMov, "movie"));
-      // API Tv
-      axios.get("https://api.themoviedb.org/3/search/tv?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
-      .then(rispostaTv => this.updateArray(rispostaTv, "tv"));
+      this.updateArray("movie")
+      this.updateArray("tv")
       this.searchKey = "";
     },
 
-    updateArray: function(apiresp, movieTv) {
-      apiresp.data.results.forEach((item) => {
-        Vue.set(item, "type", movieTv)
-        Vue.set(item, "stars", Math.ceil(item.vote_average / 2))
-        Vue.set(item, "nostars", (5 - Math.ceil(item.vote_average / 2)))
+    updateArray(movieTv) {
+      axios.get("https://api.themoviedb.org/3/search/" + movieTv + "?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
+      .then(resp => {
+        resp.data.results.forEach(item => {
+          Vue.set(item, "type", movieTv)
+          Vue.set(item, "stars", Math.ceil(item.vote_average / 2))
+          Vue.set(item, "nostars", (5 - Math.ceil(item.vote_average / 2)))
+        })
+        this.arrayFilm = [...this.arrayFilm, ...resp.data.results]
       })
-      this.arrayFilm = [...this.arrayFilm, ...apiresp.data.results]
-    }
+    },
+
+    replacePoster(event) {
+      event.target.src = "img/randomPoster.jpg"
+    },
 
   },
 
