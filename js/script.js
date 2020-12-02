d9d99b185fad1c9942c59183112f30dc7de9a636
SimoneMicheletti <sm.micheletti@gmail.com>
2020-12-02 23:37:25 +0100

    Correzione stelle e aggiunte pagine

--- js/script.js
+++ js/script.js
@@ -3,25 +3,30 @@ var app = new Vue ({
 
   data: {
     arrayFilm: [],
+    page: 1,
+    totalPages: 1,
+    maxVote: 5,
+    query: "",
     searchKey: ""
   },
 
   methods: {
 
     searchFilm() {
-      this.arrayFilm = [],
+      this.arrayFilm = [];
+      this.searchKey != "" ? this.query = this.searchKey : this.query
+      this.searchKey = "";
       this.updateArray("movie")
       this.updateArray("tv")
-      this.searchKey = "";
     },
 
     updateArray(movieTv) {
-      axios.get("https://api.themoviedb.org/3/search/" + movieTv + "?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
+      axios.get("https://api.themoviedb.org/3/search/" + movieTv + "?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.query + "&page=" + this.page)
       .then(resp => {
+        this.totalPages < resp.data.total_pages ? this.totalPages = resp.data.total_pages : this.total_pages
         resp.data.results.forEach(item => {
           Vue.set(item, "type", movieTv)
-          Vue.set(item, "stars", Math.ceil(item.vote_average / 2))
-          Vue.set(item, "nostars", (5 - Math.ceil(item.vote_average / 2)))
+          Vue.set(item, "vote_average", Math.ceil(item.vote_average / 2))
         })
         this.arrayFilm = [...this.arrayFilm, ...resp.data.results]
       })
@@ -30,6 +35,16 @@ var app = new Vue ({
     replacePoster(event) {
       event.target.src = "img/randomPoster.jpg"
     },
+
+    nextPage() {
+      this.page++
+      this.searchFilm()
+    },
+
+    prevPage() {
+      this.page--
+      this.searchFilm()
+    }
 
   },
 
