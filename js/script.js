d122e48c7528394a83b01d3e8fcae22ce1b610e4
SimoneMicheletti <sm.micheletti@gmail.com>
2020-12-02 19:13:57 +0100

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var app = new Vue({
-
+var app = new Vue ({
   el: "#app",
 
   data: {
@@ -8,11 +7,27 @@ var app = new Vue({
   },
 
   methods: {
+
     searchFilm: function() {
+      this.arrayFilm = [],
+      // API Movie
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
-      .then(risposta => this.arrayFilm = risposta.data.results);
+      .then(rispostaMov => this.updateArray(rispostaMov, "movie"));
+      // API Tv
+      axios.get("https://api.themoviedb.org/3/search/tv?api_key=3c7831140b3840fb6c05d908251a82a8&query=" + this.searchKey)
+      .then(rispostaTv => this.updateArray(rispostaTv, "tv"));
       this.searchKey = "";
+    },
+
+    updateArray: function(apiresp, movieTv) {
+      apiresp.data.results.forEach((item) => {
+        Vue.set(item, "type", movieTv)
+        Vue.set(item, "stars", Math.ceil(item.vote_average / 2))
+        Vue.set(item, "nostars", (5 - Math.ceil(item.vote_average / 2)))
+      })
+      this.arrayFilm = [...this.arrayFilm, ...apiresp.data.results]
     }
-  }
+
+  },
 
 })
